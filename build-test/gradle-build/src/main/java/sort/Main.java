fceeffcf771c0c792d7eaf231cdb2e0fedba8938
letkovmaxim <79664117+letkovmaxim@users.noreply.github.com>
2022-06-06 21:39:48 +0300

    Add files via upload

---
+++ build-test/gradle-build/src/main/java/sort/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        ArrayBubble array = new ArrayBubble(5); //Создаем массив array на 5 элементов
+
+        array.into(163);       //заполняем массив
+        array.into(300);
+        array.into(184);
+        array.into(191);
+        array.into(174);
+
+        array.printer();            //выводим элементы до сортировки
+        array.bubbleSorter();       //ИСПОЛЬЗУЕМ ПУЗЫРЬКОВУЮ СОРТИРОВКУ
+        array.printer();            //снова выводим отсортированный йсписок
+    }
+}
